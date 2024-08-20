b10b3a166cf963b11dbf30532f289e36147209a5
samirkumarbehara <samirkumarbehara1223@gmail.com>
2024-08-20 16:10:54 +0530

    push1

---
+++ sub.py
@@ -0,0 +1,16 @@
+#logic-1
+
+# def sub(a,b):
+#     return a-b
+#     return a+b
+
+#logic 2
+# def sub(a,b):
+#     return a-b
+#
+# def add(c, d):
+#     return c+d
+
+# logic 3
+def add(a,b):
+    return a+b
